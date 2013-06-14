374727bff265570b39a05cd1c73cb6987d0c6af5
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2013-06-14 13:10:03 +0300

    Added full project files

---
+++ gen/R.java.d
@@ -0,0 +1,11 @@
+/Programming/Android/Workspace/Framework/gen/com/mpack/Framework/R.java \
+ : /Programming/Android/Workspace/Framework/res/values/strings.xml \
+/Programming/Android/Workspace/Framework/res/drawable-hdpi/ic_launcher.png \
+/Programming/Android/Workspace/Framework/res/drawable-mdpi/ic_launcher.png \
+/Programming/Android/Workspace/Framework/res/drawable-xhdpi/ic_launcher.png \
+/Programming/Android/Workspace/Framework/res/drawable-ldpi/ic_launcher.png \
+/Programming/Android/Workspace/Framework/bin/res/drawable-hdpi/ic_launcher.png \
+/Programming/Android/Workspace/Framework/bin/res/drawable-mdpi/ic_launcher.png \
+/Programming/Android/Workspace/Framework/bin/res/drawable-xhdpi/ic_launcher.png \
+/Programming/Android/Workspace/Framework/bin/res/drawable-ldpi/ic_launcher.png \
+/Programming/Android/Workspace/Framework/AndroidManifest.xml \
--- jni/Android.mk
+++ jni/Android.mk
@@ -8,6 +8,10 @@ LOCAL_LDLIBS    := -landroid -llog -lEGL -lGLESv2 -lOpenSLES
 
 LOCAL_STATIC_LIBRARIES := android_native_app_glue png
 
+$(info **********************************************************************)
+$(info $(LOCAL_SRC_FILES))
+$(info **********************************************************************)
+
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,android/native_app_glue)
