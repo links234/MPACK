42a0a612b56c86d9a144c228b1e00dff17ed5065
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2013-07-05 15:16:54 +0300

    Implemented Player from Object

--- jni/Game/Player.cpp
+++ jni/Game/Player.cpp
@@ -6,23 +6,12 @@ Player::Player()
 
 void Player::Update(GLfloat delta)
 {
-
-	m_velocity+=m_acceleration*delta;
+	Object::Update(delta);
 }
 
 void Player::Render()
 {
-
-}
-
-void Player::SetFriction(GLfloat friction)
-{
-	m_friction=friction;
-}
-
-void Player::SetMinFriction(GLfloat minFriction)
-{
-	m_minFriction=minFriction;
+	Object::Render();
 }
 
 Player::~Player()
