7719bd369a7240d38622d48c0653571d32667502
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2013-07-18 01:37:45 +0300

    Updated math header

--- jni/Math/Maths.hpp
+++ jni/Math/Maths.hpp
@@ -10,6 +10,7 @@
 #define MATHS_HPP
 
 #include "Common.hpp"
+#include "TransformState2.hpp"
 #include "Matrix3.hpp"
 #include "Matrix4.hpp"
 #include "Vector2.hpp"
@@ -22,17 +23,34 @@
 
 namespace Math
 {
-	typedef Vector2<float> 	Vector2f;
-	typedef Vector3<float> 	Vector3f;
-	typedef Vector4<float> 	Vector4f;
+	typedef TransformState2<float>		TransformState2f;
 
-	typedef Matrix3<float> 	Matrix3f;
-	typedef Matrix4<float> 	Matrix4f;
+	typedef Vector2<float> 			Vector2f;
+	typedef Vector3<float> 			Vector3f;
+	typedef Vector4<float> 			Vector4f;
 
-	typedef Plane<float>	Planef;
+	typedef Matrix3<float> 			Matrix3f;
+	typedef Matrix4<float> 			Matrix4f;
 
-	typedef AABB1D<float>	AABB1Df;
-	typedef AABB2D<float> 	AABB2Df;
+	typedef Plane<float>				Planef;
+
+	typedef AABB1D<float>				AABB1Df;
+	typedef AABB2D<float> 				AABB2Df;
+
+
+	typedef TransformState2<double>	TransformState2d;
+
+	typedef Vector2<double> 			Vector2d;
+	typedef Vector3<double> 			Vector3d;
+	typedef Vector4<double> 			Vector4d;
+
+	typedef Matrix3<double> 			Matrix3d;
+	typedef Matrix4<double> 			Matrix4d;
+
+	typedef Plane<double>				Planed;
+
+	typedef AABB1D<double>				AABB1Dd;
+	typedef AABB2D<double> 			AABB2Dd;
 }
 
 #endif
