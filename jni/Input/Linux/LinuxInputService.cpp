fa2a0ab43498dd60f5ee9d966a45e0f1a94661af
links234 <murtaza_alexandru73@yahoo.com>
2014-01-19 13:42:27 +0200

    Fixed Linux mouse input crashing when window not initialized

--- jni/Input/Linux/LinuxInputService.cpp
+++ jni/Input/Linux/LinuxInputService.cpp
@@ -2,8 +2,13 @@
 
 #include "LinuxInputService.hpp"
 
+#include <stdexcept>
+#include <cassert>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
 #include <algorithm>
 #include "Global.hpp"
+#include "LinuxEventLoop.hpp"
 
 using namespace std;
 using namespace Math;
@@ -22,7 +27,42 @@ namespace Core
 
 	void LinuxInputService::Update()
 	{
+		std::swap(m_currMouse,m_lastMouse);
 
+
+		Window *m_window=NULL;
+		if(Global::pEventLoop)
+		{
+			m_window=(Window*)(Global::pEventLoop->GetWindowHandle());
+		}
+
+		if(m_window)
+		{
+			Display *dpy;
+			Window root;
+			Window ret_root;
+			Window ret_child;
+			int root_x;
+			int root_y;
+			int winX;
+			int winY;
+			unsigned int mask;
+
+			dpy = XOpenDisplay(NULL);
+			root = XDefaultRootWindow(dpy);
+
+			if(XQueryPointer(dpy, *m_window, &ret_root, &ret_child, &root_x, &root_y,
+						 &winX, &winY, &mask))
+			{
+				m_currMouse->Pos.x = winX;
+				m_currMouse->Pos.y = winY;
+
+				m_currMouse->Button.Left = (mask & Button1Mask) ? true : false;
+				m_currMouse->Button.Right = (mask & Button2Mask) ? true : false;
+			}
+
+			XCloseDisplay(dpy);
+		}
 	}
 
 	void LinuxInputService::Reset()
