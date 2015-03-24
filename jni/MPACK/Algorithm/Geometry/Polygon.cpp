d34a8cccba28275ce8ad7ce6ee326cbe0cdbac8a
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-03-24 20:35:33 +0200

    Source tree restructure of Algorithm package

---
+++ jni/MPACK/Algorithm/Geometry/Polygon.cpp
@@ -0,0 +1,10 @@
+#ifndef MPACK_ALGORITHM_HPP
+#define MPACK_ALGORITHM_HPP
+
+#include "IndexedSkipList.hpp"
+#include "StaticStack.hpp"
+#include "StaticQueue.hpp"
+#include "SearchList.hpp"
+#include "AVL.hpp"
+
+#endif
