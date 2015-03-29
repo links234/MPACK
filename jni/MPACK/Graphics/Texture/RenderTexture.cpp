08bcee47c6eb63e7b268ddc7b10d5861e1859d24
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-03-29 04:05:40 +0300

    Fixed framebuffer bug (crash on Nexus 7)

--- jni/MPACK/Graphics/Texture/RenderTexture.cpp
+++ jni/MPACK/Graphics/Texture/RenderTexture.cpp
@@ -51,6 +51,7 @@ namespace MPACK
 			{
 				m_depthTex=new Texture2D();
 			}
+			/*
 			GL_CHECK( glGenTextures(1, &m_depthTex->m_texId) );
 			GL_CHECK( glBindTexture(GL_TEXTURE_2D, m_depthTex->m_texId) );
 			GL_CHECK( glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR) );
@@ -60,14 +61,23 @@ namespace MPACK
 			GL_CHECK( glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, width, height, 0,
 						 GL_DEPTH_COMPONENT, GL_UNSIGNED_SHORT, NULL) );
 			GL_CHECK( glBindTexture(GL_TEXTURE_2D, 0) );
+			 */
+
+			GLuint depthRenderbuffer;
+			GL_CHECK( glGenRenderbuffers(1, &depthRenderbuffer) );
+			GL_CHECK( glBindRenderbuffer(GL_RENDERBUFFER, depthRenderbuffer) );
+			GL_CHECK( glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, width, height) );
+
 
 			GL_CHECK( glGenFramebuffers(1, &m_fboId) );
 			GL_CHECK( glBindFramebuffer(GL_FRAMEBUFFER, m_fboId) );
 
 			GL_CHECK( glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
 									  GL_TEXTURE_2D, m_colorTex->m_texId, 0) );
-			GL_CHECK( glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
-									  GL_TEXTURE_2D, m_depthTex->m_texId, 0) );
+			//GL_CHECK( glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
+			//						  GL_TEXTURE_2D, m_depthTex->m_texId, 0) );
+
+			GL_CHECK ( glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderbuffer) );
 
 			GL_CHECK( glBindFramebuffer(GL_FRAMEBUFFER, 0) );
 
