4f3707d49d46f293ea0fcf341399bb7da546bdbc
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-03-30 02:18:25 +0300

    Fixed depthbuffer generation bug

--- jni/MPACK/Graphics/Texture/RenderTexture.cpp
+++ jni/MPACK/Graphics/Texture/RenderTexture.cpp
@@ -5,26 +5,30 @@ namespace MPACK
 	namespace Graphics
 	{
 		RenderTexture::RenderTexture()
-			: m_fboId(0), m_depthTex(NULL), m_colorTex(NULL)
+			: m_FBOId(0), m_colorTex(NULL), m_depthRBOId(0)
 		{
 		}
+
 		RenderTexture::~RenderTexture()
 		{
-			GL_CHECK( glDeleteFramebuffers(1,&m_fboId) );
+			if(m_FBOId)
+			{
+				GL_CHECK( glDeleteFramebuffers(1,&m_FBOId) );
+			}
 			if(m_colorTex)
 			{
 				delete m_colorTex;
 				m_colorTex = NULL;
 			}
-			if(m_depthTex)
+			if(m_depthRBOId)
 			{
-				delete m_depthTex;
-				m_depthTex = NULL;
+				GL_CHECK( glDeleteRenderbuffers(1,&m_depthRBOId) );
 			}
 		}
+
 		void RenderTexture::Init(GLuint width, GLuint height)
 		{
-			GL_CHECK( glDeleteFramebuffers(1,&m_fboId) );
+			GL_CHECK( glDeleteFramebuffers(1,&m_FBOId) );
 			if(m_colorTex)
 			{
 				GL_CHECK( glDeleteTextures(1,&m_colorTex->m_texId) );
@@ -43,41 +47,22 @@ namespace MPACK
 						 GL_RGBA, GL_UNSIGNED_BYTE, 0) );
 			GL_CHECK( glBindTexture(GL_TEXTURE_2D, 0) );
 
-			if(m_depthTex)
+			if(m_depthRBOId)
 			{
-				GL_CHECK( glDeleteTextures(1,&m_depthTex->m_texId) );
+				GL_CHECK( glDeleteRenderbuffers(1,&m_depthRBOId) );
+				m_depthRBOId=0;
 			}
-			else
-			{
-				m_depthTex=new Texture2D();
-			}
-			/*
-			GL_CHECK( glGenTextures(1, &m_depthTex->m_texId) );
-			GL_CHECK( glBindTexture(GL_TEXTURE_2D, m_depthTex->m_texId) );
-			GL_CHECK( glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR) );
-			GL_CHECK( glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR) );
-			GL_CHECK( glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE) );
-			GL_CHECK( glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE) );
-			GL_CHECK( glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, width, height, 0,
-						 GL_DEPTH_COMPONENT, GL_UNSIGNED_SHORT, NULL) );
-			GL_CHECK( glBindTexture(GL_TEXTURE_2D, 0) );
-			 */
-
-			GLuint depthRenderbuffer;
-			GL_CHECK( glGenRenderbuffers(1, &depthRenderbuffer) );
-			GL_CHECK( glBindRenderbuffer(GL_RENDERBUFFER, depthRenderbuffer) );
+			GL_CHECK( glGenRenderbuffers(1, &m_depthRBOId) );
+			GL_CHECK( glBindRenderbuffer(GL_RENDERBUFFER, m_depthRBOId) );
 			GL_CHECK( glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, width, height) );
 
 
-			GL_CHECK( glGenFramebuffers(1, &m_fboId) );
-			GL_CHECK( glBindFramebuffer(GL_FRAMEBUFFER, m_fboId) );
+			GL_CHECK( glGenFramebuffers(1, &m_FBOId) );
+			GL_CHECK( glBindFramebuffer(GL_FRAMEBUFFER, m_FBOId) );
 
 			GL_CHECK( glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
 									  GL_TEXTURE_2D, m_colorTex->m_texId, 0) );
-			//GL_CHECK( glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
-			//						  GL_TEXTURE_2D, m_depthTex->m_texId, 0) );
-
-			GL_CHECK ( glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderbuffer) );
+			GL_CHECK( glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, m_depthRBOId) );
 
 			GL_CHECK( glBindFramebuffer(GL_FRAMEBUFFER, 0) );
 
@@ -85,17 +70,16 @@ namespace MPACK
 			GL_CHECK( status = glCheckFramebufferStatus(GL_FRAMEBUFFER) );
 			if ( status == GL_FRAMEBUFFER_COMPLETE )
 			{
-				LOGI("FBO successfully created!");
-			}
-			else
-			{
 				LOGE("ERROR: Failed to init FBO");
 			}
 		}
+
 		void RenderTexture::Bind()
 		{
-			GL_CHECK( glBindFramebuffer(GL_FRAMEBUFFER, m_fboId) );
+			GL_CHECK( glBindFramebuffer(GL_FRAMEBUFFER, m_FBOId) );
+			GL_CHECK( glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) );
 		}
+
 		void RenderTexture::Unbind()
 		{
 			GL_CHECK( glBindFramebuffer(GL_FRAMEBUFFER, 0) );
