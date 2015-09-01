23b4e269178bd38910389fdba05d5d0abb8f344f
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-09-01 23:45:53 +0300

    Travis: libxxf86vm-dev fix

--- scripts/config_linux.sh
+++ scripts/config_linux.sh
@@ -5,5 +5,6 @@ sudo apt-get install build-essential
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 sudo apt-get install g++-4.7
+sudo apt-get install libxxf86vm-dev
 sudo apt-get install libgl1-mesa-dev libglu1-mesa-dev
 sudo apt-get install libpng-dev
