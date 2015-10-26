8e8c036dfc6756cc8551071cd27a43d9bf961a4c
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-10-26 02:28:24 +0200

    Add unified mouse-to-finger emulation for desktop builds

--- jni/MPACK/Input/Desktop/Linux/LinuxInputService.cpp
+++ jni/MPACK/Input/Desktop/Linux/LinuxInputService.cpp
@@ -2,8 +2,13 @@
 
 #include "LinuxInputService.hpp"
 
+#include "InputService.hpp"
 #include "LinuxMouseInterface.hpp"
 #include "LinuxKeyboardInterface.hpp"
+
+using namespace std;
+using namespace MPACK;
+using namespace MPACK::Core;
 
 namespace MPACK
 {
@@ -11,6 +16,7 @@ namespace MPACK
 	{
 		LinuxInputService::LinuxInputService()
 		{
+			m_pFinger = new Finger;
 			m_pMouse = new LinuxMouseInterface;
 			m_pKeyboard = new LinuxKeyboardInterface;
 			Reset();
@@ -18,12 +24,17 @@ namespace MPACK
 
 		LinuxInputService::~LinuxInputService()
 		{
+			delete m_pFinger;
+			delete m_pMouse;
+			delete m_pKeyboard;
 		}
 
 		void LinuxInputService::Update()
 		{
 			m_pMouse->Update();
 			m_pKeyboard->Update();
+
+			EmulateTouchscreenFinger();
 		}
 
 		void LinuxInputService::Reset()
@@ -40,6 +51,96 @@ namespace MPACK
 		KeyboardInterface* LinuxInputService::GetKeyboard() const
 		{
 			return m_pKeyboard;
+		}
+
+		void LinuxInputService::EmulateTouchscreenFinger()
+		{
+			if(m_pMouse->ButtonPressed(MBC_LEFT))
+			{
+				m_pFinger->m_pos = m_pMouse->GetPosition();
+			}
+
+			if(m_pMouse->ButtonDown(MBC_LEFT))
+			{
+				m_pFinger->m_flag = Finger::FREE;
+				for(vector<Param2PtrCallbackStruct>::iterator it=m_callbackFunc_FDOWN.begin();it!=m_callbackFunc_FDOWN.end();++it)
+				{
+					it->function(it->param1,(void*)(m_pFinger));
+				}
+			}
+			else if(m_pMouse->ButtonUp(MBC_LEFT))
+			{
+				for(vector<Param2PtrCallbackStruct>::iterator it=m_callbackFunc_FUP.begin();it!=m_callbackFunc_FUP.end();++it)
+				{
+					it->function(it->param1,(void*)(m_pFinger));
+				}
+			}
+
+			if(m_pKeyboard->KeyPressed(KC_ESCAPE))
+			{
+				for(vector<Param1PtrCallbackStruct>::iterator it=m_callbackFunc_KEYBACK.begin();it!=m_callbackFunc_KEYBACK.end();++it)
+				{
+					(*it).function((*it).param1);
+				}
+			}
+		}
+
+		void LinuxInputService::ClearLinks()
+		{
+			m_callbackFunc_FDOWN.clear();
+			m_callbackFunc_FUP.clear();
+			m_callbackFunc_KEYBACK.clear();
+		}
+
+		void LinuxInputService::Link_FUP(const Param2PtrCallbackStruct &link)
+		{
+			m_callbackFunc_FUP.push_back(link);
+		}
+
+		void LinuxInputService::Link_FDOWN(const Param2PtrCallbackStruct &link)
+		{
+			m_callbackFunc_FDOWN.push_back(link);
+		}
+
+		void LinuxInputService::Link_KEYBACK(const Param1PtrCallbackStruct &link)
+		{
+			m_callbackFunc_KEYBACK.push_back(link);
+		}
+
+		void LinuxInputService::UnLink_FUP(const Param2PtrCallbackStruct &link)
+		{
+			for(vector<Param2PtrCallbackStruct>::iterator it=m_callbackFunc_FUP.begin();it!=m_callbackFunc_FUP.end();++it)
+			{
+				if(*it==link)
+				{
+					m_callbackFunc_FUP.erase(it);
+					return;
+				}
+			}
+		}
+
+		void LinuxInputService::UnLink_FDOWN(const Param2PtrCallbackStruct &link)
+		{
+			for(vector<Param2PtrCallbackStruct>::iterator it=m_callbackFunc_FDOWN.begin();it!=m_callbackFunc_FDOWN.end();++it)
+			{
+				if(*it==link)
+				{
+					m_callbackFunc_FDOWN.erase(it);
+					return;
+				}
+			}
+		}
+
+		void LinuxInputService::UnLink_KEYBACK(const Param1PtrCallbackStruct &link)
+		{
+			for(vector<Param1PtrCallbackStruct>::iterator it=m_callbackFunc_KEYBACK.begin();it!=m_callbackFunc_KEYBACK.end();++it)
+			{
+				if(*it==link)
+				{
+					m_callbackFunc_KEYBACK.erase(it);
+					return;
+				}
+			}
 		}
 	}
 }
--- jni/MPACK/Input/Desktop/Linux/LinuxInputService.hpp
+++ jni/MPACK/Input/Desktop/Linux/LinuxInputService.hpp
@@ -11,6 +11,14 @@ namespace MPACK
 {
 	namespace Input
 	{
+		struct Finger;
+	}
+}
+
+namespace MPACK
+{
+	namespace Input
+	{
 		class LinuxInputService
 		{
 		public:
@@ -23,9 +31,28 @@ namespace MPACK
 			MouseInterface*		GetMouse()		const;
 			KeyboardInterface*	GetKeyboard() 	const;
 
+			void ClearLinks();
+
+			void Link_FUP(const Core::Param2PtrCallbackStruct &link);
+			void Link_FDOWN(const Core::Param2PtrCallbackStruct &link);
+			void Link_KEYBACK(const Core::Param1PtrCallbackStruct &link);
+
+			void UnLink_FUP(const Core::Param2PtrCallbackStruct &link);
+			void UnLink_FDOWN(const Core::Param2PtrCallbackStruct &link);
+			void UnLink_KEYBACK(const Core::Param1PtrCallbackStruct &link);
+
 		private:
+			void EmulateTouchscreenFinger();
+
+			Finger				*m_pFinger;
+
 			MouseInterface		*m_pMouse;
 			KeyboardInterface	*m_pKeyboard;
+
+			std::vector<Core::Param2PtrCallbackStruct> m_callbackFunc_FUP;
+			std::vector<Core::Param2PtrCallbackStruct> m_callbackFunc_FDOWN;
+
+			std::vector<Core::Param1PtrCallbackStruct> m_callbackFunc_KEYBACK;
 		};
 	}
 }
