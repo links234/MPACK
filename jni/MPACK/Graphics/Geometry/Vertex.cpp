4d0b9f6e764c771d247684489b2c3fa5d579f56e
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-11-08 00:32:44 +0200

    Remove comment header with old license

--- jni/MPACK/Graphics/Geometry/Vertex.cpp
+++ jni/MPACK/Graphics/Geometry/Vertex.cpp
@@ -1,10 +1,2 @@
-/*
-	This file is owned by Murtaza Alexandru and may not be distributed, edited or used without written permission of the owner
-	When using this work you accept to keep this header
-	E-mails from murtaza_alexandru73@yahoo.com with permissions can be seen as valid.
-*/
-
-
-
 #include "Vertex.hpp"
 
--- jni/MPACK/Graphics/Shader/ShaderUniform.cpp
+++ jni/MPACK/Graphics/Shader/ShaderUniform.cpp
@@ -1,11 +1,3 @@
-/*
-	This file is owned by Murtaza Alexandru and may not be distributed, edited or used without written permission of the owner
-	When using this work you accept to keep this header
-	E-mails from murtaza_alexandru73@yahoo.com with permissions can be seen as valid.
-*/
-
-
-
 #include "ShaderUniform.hpp"
 
 using namespace MPACK::Math;
