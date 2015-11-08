8c8e4079c9ad643df994d9875e0e317417e83c2f
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-11-08 02:36:53 +0200

    Add finger emulation for Windows + fix input service memory leak on Windows

--- jni/MPACK/Input/Desktop/Windows/WindowsInputService.cpp
+++ jni/MPACK/Input/Desktop/Windows/WindowsInputService.cpp
@@ -11,6 +11,7 @@ namespace MPACK
 	{
 		WindowsInputService::WindowsInputService()
 		{
+			m_pFinger = new Finger;
 			m_pMouse = new WindowsMouseInterface;
 			m_pKeyboard = new WindowsKeyboardInterface;
 			Reset();
@@ -18,12 +19,17 @@ namespace MPACK
 
 		WindowsInputService::~WindowsInputService()
 		{
+			delete m_pFinger;
+			delete m_pMouse;
+			delete m_pKeyboard;
 		}
 
 		void WindowsInputService::Update()
 		{
 			m_pMouse->Update();
 			m_pKeyboard->Update();
+
+			EmulateTouchscreenFinger();
 		}
 
 		void WindowsInputService::Reset()
@@ -40,6 +46,97 @@ namespace MPACK
 		KeyboardInterface* WindowsInputService::GetKeyboard() const
 		{
 			return m_pKeyboard;
+		}
+
+		void WindowsInputService::EmulateTouchscreenFinger()
+		{
+			if(m_pMouse->ButtonPressed(MBC_LEFT))
+			{
+				m_pFinger->m_pos = m_pMouse->GetPosition();
+			}
+
+			if(m_pMouse->ButtonDown(MBC_LEFT))
+			{
+				m_pFinger->m_flag = Finger::FREE;
+				m_pFinger->m_pos = m_pMouse->GetPosition();
+				for(vector<Param2PtrCallbackStruct>::iterator it=m_callbackFunc_FDOWN.begin();it!=m_callbackFunc_FDOWN.end();++it)
+				{
+					it->function(it->param1,(void*)(m_pFinger));
+				}
+			}
+			else if(m_pMouse->ButtonUp(MBC_LEFT))
+			{
+				for(vector<Param2PtrCallbackStruct>::iterator it=m_callbackFunc_FUP.begin();it!=m_callbackFunc_FUP.end();++it)
+				{
+					it->function(it->param1,(void*)(m_pFinger));
+				}
+			}
+
+			if(m_pKeyboard->KeyPressed(KC_ESCAPE))
+			{
+				for(vector<Param1PtrCallbackStruct>::iterator it=m_callbackFunc_KEYBACK.begin();it!=m_callbackFunc_KEYBACK.end();++it)
+				{
+					(*it).function((*it).param1);
+				}
+			}
+		}
+
+		void WindowsInputService::ClearLinks()
+		{
+			m_callbackFunc_FDOWN.clear();
+			m_callbackFunc_FUP.clear();
+			m_callbackFunc_KEYBACK.clear();
+		}
+
+		void WindowsInputService::Link_FUP(const Param2PtrCallbackStruct &link)
+		{
+			m_callbackFunc_FUP.push_back(link);
+		}
+
+		void WindowsInputService::Link_FDOWN(const Param2PtrCallbackStruct &link)
+		{
+			m_callbackFunc_FDOWN.push_back(link);
+		}
+
+		void WindowsInputService::Link_KEYBACK(const Param1PtrCallbackStruct &link)
+		{
+			m_callbackFunc_KEYBACK.push_back(link);
+		}
+
+		void WindowsInputService::UnLink_FUP(const Param2PtrCallbackStruct &link)
+		{
+			for(vector<Param2PtrCallbackStruct>::iterator it=m_callbackFunc_FUP.begin();it!=m_callbackFunc_FUP.end();++it)
+			{
+				if(*it==link)
+				{
+					m_callbackFunc_FUP.erase(it);
+					return;
+				}
+			}
+		}
+
+		void WindowsInputService::UnLink_FDOWN(const Param2PtrCallbackStruct &link)
+		{
+			for(vector<Param2PtrCallbackStruct>::iterator it=m_callbackFunc_FDOWN.begin();it!=m_callbackFunc_FDOWN.end();++it)
+			{
+				if(*it==link)
+				{
+					m_callbackFunc_FDOWN.erase(it);
+					return;
+				}
+			}
+		}
+
+		void WindowsInputService::UnLink_KEYBACK(const Param1PtrCallbackStruct &link)
+		{
+			for(vector<Param1PtrCallbackStruct>::iterator it=m_callbackFunc_KEYBACK.begin();it!=m_callbackFunc_KEYBACK.end();++it)
+			{
+				if(*it==link)
+				{
+					m_callbackFunc_KEYBACK.erase(it);
+					return;
+				}
+			}
 		}
 	}
 }
--- jni/MPACK/Input/Desktop/Windows/WindowsInputService.hpp
+++ jni/MPACK/Input/Desktop/Windows/WindowsInputService.hpp
@@ -23,10 +23,28 @@ namespace MPACK
 			MouseInterface*		GetMouse() const;
 			KeyboardInterface* 	GetKeyboard() const;
 
+			void ClearLinks();
+
+			void Link_FUP(const Core::Param2PtrCallbackStruct &link);
+			void Link_FDOWN(const Core::Param2PtrCallbackStruct &link);
+			void Link_KEYBACK(const Core::Param1PtrCallbackStruct &link);
+
+			void UnLink_FUP(const Core::Param2PtrCallbackStruct &link);
+			void UnLink_FDOWN(const Core::Param2PtrCallbackStruct &link);
+			void UnLink_KEYBACK(const Core::Param1PtrCallbackStruct &link);
+
 		private:
+			void EmulateTouchscreenFinger();
+
+			Finger				*m_pFinger;
 
 			MouseInterface		*m_pMouse;
 			KeyboardInterface	*m_pKeyboard;
+
+			std::vector<Core::Param2PtrCallbackStruct> m_callbackFunc_FUP;
+			std::vector<Core::Param2PtrCallbackStruct> m_callbackFunc_FDOWN;
+
+			std::vector<Core::Param1PtrCallbackStruct> m_callbackFunc_KEYBACK;
 		};
 	}
 }
