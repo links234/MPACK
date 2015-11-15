de8487f01479cac2ce97b6a469c9ba64abae2b3b
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-11-16 01:41:33 +0200

    UI: add new helper functions

--- jni/MPACK/Graphics/UI/UIHelper.cpp
+++ jni/MPACK/Graphics/UI/UIHelper.cpp
@@ -1,8 +1,10 @@
 #include "UIHelper.hpp"
 
+#include "Types.hpp"
 #include "Render.hpp"
 #include "Anchor.hpp"
 
+using namespace std;
 using namespace MPACK::Math;
 using namespace MPACK::Graphics;
 
@@ -10,6 +12,9 @@ namespace MPACK
 {
 	namespace UI
 	{
+		double defaultOriginalWidth = 0.0;
+		double defaultOriginalHeight = 0.0;
+
 		double Fit(const double &currentGlobalSize, const double &originalSize, const double &originalGlobalSize)
 		{
 			return originalSize/originalGlobalSize*currentGlobalSize;
@@ -20,9 +25,19 @@ namespace MPACK
 			return originalSize/originalGlobalSize*Render::GetScreenWidth();
 		}
 
+		double FitWidth(const double &originalSize)
+		{
+			return FitWidth(originalSize, defaultOriginalWidth);
+		}
+
 		double FitHeight(const double &originalSize, const double &originalGlobalSize)
 		{
 			return originalSize/originalGlobalSize*Render::GetScreenHeight();
+		}
+
+		double FitHeight(const double &originalSize)
+		{
+			return FitHeight(originalSize, defaultOriginalHeight);
 		}
 
 		void FitByWidth(double &width, double &height, const double &originalWidth, const double &originalHeight)
@@ -37,6 +52,36 @@ namespace MPACK
 			double ratio=width/height;
 			height=originalHeight;
 			width=height*ratio;
+		}
+
+		void SetDefaultOriginalWidth(const double &width)
+		{
+			defaultOriginalWidth = width;
+		}
+
+		void SetDefaultOriginalHeight(const double &height)
+		{
+			defaultOriginalHeight = height;
+		}
+
+		double GetDefaultOriginalWidth()
+		{
+			return defaultOriginalWidth;
+		}
+
+		double GetDefaultOriginalHeight()
+		{
+			return defaultOriginalHeight;
+		}
+
+		double FitForMax(const double &originalSize)
+		{
+			return max(FitWidth(originalSize), FitHeight(originalSize));
+		}
+
+		double FitForMin(const double &originalSize)
+		{
+			return min(FitWidth(originalSize), FitHeight(originalSize));
 		}
 
 		Anchor ScreenToAnchor(const MPACK::Math::Vector2f &position)
--- jni/MPACK/Graphics/UI/UIHelper.hpp
+++ jni/MPACK/Graphics/UI/UIHelper.hpp
@@ -17,9 +17,19 @@ namespace MPACK
 	{
 		double Fit(const double &currentGlobalSize, const double &originalSize, const double &originalGlobalSize);
 		double FitWidth(const double &originalSize, const double &originalGlobalSize);
+		double FitWidth(const double &originalSize);
 		double FitHeight(const double &originalSize, const double &originalGlobalSize);
+		double FitHeight(const double &originalSize);
 		void FitByWidth(double &width, double &height, const double &originalWidth, const double &originalHeight);
 		void FitByHeight(double &width, double &height, const double &originalWidth, const double &originalHeight);
+
+		void SetDefaultOriginalWidth(const double &width);
+		void SetDefaultOriginalHeight(const double &height);
+		double GetDefaultOriginalWidth();
+		double GetDefaultOriginalHeight();
+
+		double FitForMax(const double &originalSize);
+		double FitForMin(const double &originalSize);
 
 		Anchor ScreenToAnchor(const MPACK::Math::Vector2f &position);
 	}
