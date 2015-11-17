463cb12279b6cae195dd108ee8730999132e6e7e
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-11-18 01:55:29 +0200

    scripts: config_linux: add --yes --force-yes for apt-get

--- scripts/config_linux.sh
+++ scripts/config_linux.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #set -x
 
-sudo apt-get install build-essential
+sudo apt-get --yes --force-yes install build-essential
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
-sudo apt-get install g++-4.7
-sudo apt-get install libxxf86vm-dev
-sudo apt-get install libgl1-mesa-dev libglu1-mesa-dev
-sudo apt-get install libpng-dev
+sudo apt-get --yes --force-yes install g++-4.7
+sudo apt-get --yes --force-yes install libxxf86vm-dev
+sudo apt-get --yes --force-yes install libgl1-mesa-dev libglu1-mesa-dev
+sudo apt-get --yes --force-yes install libpng-dev
