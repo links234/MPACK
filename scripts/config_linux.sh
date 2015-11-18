34c9bcd49644063f58da8d6c0a29ac2496dc0d18
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-11-18 14:33:06 +0200

    scripts: config_linux: fix for proxy

--- scripts/config_linux.sh
+++ scripts/config_linux.sh
@@ -2,7 +2,7 @@
 #set -x
 
 sudo apt-get --yes --force-yes install build-essential
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo -E add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 sudo apt-get --yes --force-yes install g++-4.7
 sudo apt-get --yes --force-yes install libxxf86vm-dev
